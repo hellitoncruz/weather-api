4e3867bcd10ccc0c7b4dbe75c0a3adbfb7fafe02
Helliton <70456587+hellitoncruz@users.noreply.github.com>
2022-05-05 11:47:26 -0300

    feature: add new design (#1)

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 let search_input = document.querySelector("#search_input")
 let search_btn = document.querySelector("#search_btn")
-let tempMin = document.querySelector(".tempMin")
-let temp = document.querySelector(".temp")
-let tempMax = document.querySelector(".tempMax")
+let tempMin = document.querySelector(".tempMin-txt")
+let temp = document.querySelector(".temp-txt")
+let tempMax = document.querySelector(".tempMax-txt")
 let city_name = document.querySelector(".city_name")
+let imgIcon = document.querySelector(".imgIcon")
+let description= document.querySelector(".description-txt")
 
 let api = {
     lang: 'pt_br',
@@ -26,25 +28,31 @@ function enter(e){
         searchResults(search_input.value)
     }
 }
-// Adicionar país na frente do nome da cidade escolhida
-// Encontrar uma imagem para cada tempo (nublado, sol, chuva )
-// função para colocar a saudação (bom dia...)
-// função para selecionar a imagem do tempo com base no clima
-// função saber como está o tempo (nublado...)
 
 
-function displayData(wheater){
-    console.log(wheater)
 
-    city_name.innerText = wheater.name
 
-    temp.innerHTML = `<h2> ${Math.round(wheater.main.temp)}°C</h2>` 
-    tempMin.innerHTML = `<p>${Math.round(wheater.main.temp_min)}°C</p>` 
-    tempMax.innerHTML = `<p>${Math.round(wheater.main.temp_max)}°C</p>` 
+function displayData(weather){
+    console.log(weather)
+
+    // Adicionar país na frente do nome da cidade escolhida
+    city_name.innerText = `${weather.name}, ${weather.sys.country}`
+
+    // Encontrar uma imagem para cada tempo (nublado, sol, chuva )
+    imgIcon.innerHTML = `<img src="./icons/${weather.weather[0].icon}.png">`
+
+    // função saber como está o tempo (nublado...)
+    let tempo = weather.weather[0].description
+    description.innerText = tempo[0].toUpperCase() + tempo.substr(1);
+
+    temp.innerText = `${Math.round(weather.main.temp)}°C` 
+    tempMin.innerText = `${Math.round(weather.main.temp_min)}°C` 
+    tempMax.innerText = `${Math.round(weather.main.temp_max)}°C` 
 
     document.querySelector(".card").style.display = "flex";
 
-
+    // função para colocar a saudação (bom dia...)
+    makeGreeting()
 }
 
 
@@ -60,4 +68,21 @@ function searchResults(city) {
 }
 
 
+let greeting = document.getElementById("greeting");
+
+function makeGreeting(){
     
+    let saudacao = ["Bom dia!", "Boa tarde!", "Boa noite!", "Boa madrugada!"]
+    let dataHoje = new Date();
+    let horas = dataHoje.getHours()
+
+    if (horas >= 5 && horas <= 12) {
+        greeting.innerText = saudacao[0];
+    }
+    else if (horas > 12 && horas <= 18 ){
+        greeting.innerText = saudacao[1];
+    }
+    else if (horas > 18 && horas <= 00){
+        greeting.innerText = saudacao[2];
+    }
+}
